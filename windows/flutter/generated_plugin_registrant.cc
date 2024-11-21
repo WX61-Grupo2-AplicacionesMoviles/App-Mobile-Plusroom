d1118516eaa4b33a737b6ba74af35cbea7b9ec55
Gerald <gpserrano.18.4@gmail.com>
2024-11-21 15:56:51 -0500

    feat: merge from feature/profile

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -6,17 +6,12 @@
 
 #include "generated_plugin_registrant.h"
 
-<<<<<<< HEAD
 #include <file_selector_windows/file_selector_windows.h>
+#include <url_launcher_windows/url_launcher_windows.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   FileSelectorWindowsRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FileSelectorWindows"));
-=======
-#include <url_launcher_windows/url_launcher_windows.h>
-
-void RegisterPlugins(flutter::PluginRegistry* registry) {
   UrlLauncherWindowsRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("UrlLauncherWindows"));
->>>>>>> feature/profile
 }
